e07796dd35baed490d8cdc9959a2d35a4d6d8366
diamond vr <kaladingoodellmurphy@gmail.com>
2026-03-18 19:15:22 -0700

    I changed the name

--- GorillaTagModTemplateProject/PluginInfo.cs
+++ GorillaTagModTemplateProject/PluginInfo.cs
@@ -1,12 +1,12 @@
-﻿namespace GorillaTagModTemplateProject
+﻿namespace Slark's Glider Menu v1
 {
 	/// <summary>
 	/// This class is used to provide information about your mod to BepInEx.
 	/// </summary>
 	class PluginInfo
 	{
-		public const string GUID = "com.AuthorLowerName.gorillatag.GorillaTagLowerModTemplateProject";
-		public const string Name = "GorillaTagModTemplateProject";
+		public const string GUID = "com.AuthorLowerName.gorillatag.Slark's Glider Menu v1";
+		public const string Name = "Slark's Glider Menu v1";
 		public const string Version = "1.0.0";
 	}
 }
